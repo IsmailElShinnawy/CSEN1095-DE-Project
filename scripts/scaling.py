854346e15b03b299acd3364cbcfeb2063ee3ad76
Ismail El Shinnawy <ismailelshennawy@gmail.com>
2023-01-02 23:15:38 +0200

    format: change function names

--- scripts/scaling.py
+++ scripts/scaling.py
@@ -1,17 +1,17 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
 
-def apply_ZScore_Scaling(df, column):
+def apply_z_score_scaling(df, column):
   result = StandardScaler().fit_transform(df[[column]])
   return result
 
-def apply_MinMax(df, column):
+def apply_min_max_sclaing(df, column):
   scaler = MinMaxScaler()
   scaled_data = scaler.fit_transform(df[[column]])
   return scaled_data
 
 def scale_Data(df):
-    df['longitude'] = apply_MinMax(df,"longitude")
-    df['latitude'] = apply_MinMax(df,"latitude")
-    df['location_northing_osgr'] = apply_MinMax(df,"location_northing_osgr")
-    df['location_easting_osgr'] = apply_MinMax(df,"location_easting_osgr")
+    df['longitude'] = apply_min_max_sclaing(df,"longitude")
+    df['latitude'] = apply_min_max_sclaing(df,"latitude")
+    df['location_northing_osgr'] = apply_min_max_sclaing(df,"location_northing_osgr")
+    df['location_easting_osgr'] = apply_min_max_sclaing(df,"location_easting_osgr")
