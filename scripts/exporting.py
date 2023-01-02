2d841f550d0d54ad32dbdbea6d209c0bd8906f64
Ismail El Shinnawy <ismailelshennawy@gmail.com>
2023-01-02 22:51:50 +0200

    Revert "scripts: exporting"
    This reverts commit 524dbf24f8d647e0d8377714eb9aaf85cc9341dd.

--- scripts/exporting.py
+++
@@ -1,7 +0,0 @@
-def write_df_to_parquet_file(df, file_name):
-    file_name = file_name + ".parquet"
-    df.to_parquet(file_name, index = False)
-
-def write_df_to_csv_file(df, file_name):
-    file_name = file_name + ".csv"
-    df.to_csv(file_name, index = False)
