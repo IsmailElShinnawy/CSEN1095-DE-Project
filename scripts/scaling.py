838e016784c4cfcb85b402e1dcf27a527f75a44b
Ali Amr <aliamrr1999@gmail.com>
2023-01-02 23:10:52 +0200

    Added method to scale dataframe

--- scripts/scaling.py
+++ scripts/scaling.py
@@ -1,10 +1,17 @@
 from sklearn.preprocessing import StandardScaler
 from sklearn.preprocessing import MinMaxScaler
-def applyZScoreSaling(df, column):
+
+def apply_ZScore_Scaling(df, column):
   result = StandardScaler().fit_transform(df[[column]])
   return result
 
-def applyMinMax(df, column):
+def apply_MinMax(df, column):
   scaler = MinMaxScaler()
   scaled_data = scaler.fit_transform(df[[column]])
   return scaled_data
+
+def scale_Data(df):
+    df['longitude'] = apply_MinMax(df,"longitude")
+    df['latitude'] = apply_MinMax(df,"latitude")
+    df['location_northing_osgr'] = apply_MinMax(df,"location_northing_osgr")
+    df['location_easting_osgr'] = apply_MinMax(df,"location_easting_osgr")
